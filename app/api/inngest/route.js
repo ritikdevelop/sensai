4350f287a97e484895839e9b7f1349f269539578
ritikdevelop <hrithikrana216@gmail.com>
2025-02-07 13:05:52 +0530

    Replace generateIndustryInsights function with helloWorld in Inngest API route

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -3,8 +3,8 @@ import { serve } from "inngest/next";
 
 import { inngest } from "@/lib/inngest/client";
 import { generateIndustryInsights } from "@/lib/inngest/function";
-
+import { helloWorld } from "@/lib/inngest/function";
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [generateIndustryInsights],
+  functions: [helloWorld],
 });
